0b96381debba058ecdd29b5b426d9e747408f953
Mikael Nilsson <mikael.nilsson@elicit.se>
2020-01-28 21:43:04 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <app-nav></app-nav>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Nav from './views/layout/Nav.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components: {
+    appNav: Nav
   }
 }
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Nunito');
+@import '@/assets/scss/_variables.scss';
+@import '@/assets/scss/global.scss';
 </style>
---
+++ src/views/layout/Nav.vue
@@ -0,0 +1,98 @@
+<template>
+<div>
+    <header class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            Lets travel
+        </a>
+
+        <button href="#" id="menu-btn" @click="showMenu = !showMenu">
+            USERNAME <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</header>
+<nav id="nav-container" :class="{show: showMenu}">
+    <div id="backdrop" @click="showMenu = !showMenu">
+
+    </div>
+    <div class="nav">
+        <ul class="navbar-nav ml-auto">
+
+          <!--  @guest -->
+                <li><a class="dropdown-item" href="#">Login</a></li>
+               <!--  @if (Route::has('register')) -->
+                    <li><a class="dropdown-item" href="#">Register</a></li>
+               <!--  @endif -->
+           <!--  @endguest -->
+                <li><a class="dropdown-item" href="#">About us </a></li>
+                <li><a class="dropdown-item" href="#">Contact us </a></li>
+          <!--    @if(Auth::check())
+                    <li><a class="dropdown-item" href="{{ route('profile.edit', Auth::user()) }}">{{ __('Profile') }} </a></li>
+                    <li><a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                      style="display: none;">
+                    @csrf
+                </form></li>
+            @endif -->
+        </ul>
+    </div>
+</nav>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      showMenu: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+#menu-btn {
+    background: transparent;
+    border: 0;
+    outline: 0;
+}
+#nav-container {
+  height: 100%;
+  position: fixed;
+  width: 100%;
+  z-index: 999999;
+  margin-top: 1px;
+  display: flex;
+  transform: translateX(100%);
+  transition: 300ms ease-in all;
+}
+
+#nav-container.show {
+  transform: translateX(0);
+  transition: 300ms ease-out all;
+}
+
+#nav-container .nav {
+  flex: 0 0 80%;
+  background: #fff;
+  border-left: 1px solid #eee;
+}
+
+#backdrop {
+  flex: 0 0 20%;
+}
+
+.navbar-nav {
+  width: 50%;
+}
+
+.navbar-nav li {
+  font-size: 1.5rem;
+  line-height: 2rem;
+}
+</style>
